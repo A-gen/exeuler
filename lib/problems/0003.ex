9fcbe15c886d1640f77e3e1ccbb5150d12153242
A-gen <agen@seven-words.info>
2015-12-15 14:06:28 +0900

    Solve Problem 003 but it is inefficiency

---
+++ lib/problems/0003.ex
@@ -0,0 +1,19 @@
+defmodule ExEuler.Problems.Problem0003 do
+  @moduledoc """
+  # Largest prime factor
+  The prime factors of 13195 are 5, 7, 13 and 29.
+  What is the largest prime factor of the number 600851475143 ?
+
+  ## Example 1
+  iex> ExEuler.Problems.Problem0003.run(13195)
+  29
+
+  ## Answer
+  iex> ExEuler.Problems.Problem0003.run(600851475143)
+  6857
+  """
+
+  def run(number) do
+    ExEuler.Math.prime_factors(number) |> Enum.max
+  end
+end
--- lib/utils/math.ex
+++ lib/utils/math.ex
@@ -1,3 +1,22 @@
 defmodule ExEuler.Math do
   def fibs(first \\ {1, 1}), do: Stream.unfold(first, fn {a, b} -> {a, {b, a + b}} end)
+
+  def primes do
+    [2]
+    |> Stream.concat(
+      Stream.iterate(3, &(&1 + 2))
+      |> Stream.unfold(fn s ->
+        p = Enum.at(s, 0)
+        next = Stream.reject(s, &(rem(&1, p) == 0))
+        {p, next}
+      end)
+    )
+  end
+  def primes(max), do: primes |> Stream.take_while(&(&1 <= max))
+
+  def prime_factors(number), do: _prime_factors(number, Enum.to_list(2..(trunc :math.sqrt(number))), [])
+
+  def _prime_factors(number, [], factors), do: factors
+  def _prime_factors(number, primes = [p | _], factors) when rem(number, p) == 0, do: _prime_factors((trunc number / p), primes, factors ++ [p])
+  def _prime_factors(number, [p | t], factors), do: _prime_factors(number, t, factors)
 end
