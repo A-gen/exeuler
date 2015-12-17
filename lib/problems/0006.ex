3885f82a4a3b6f6ff694172b7f1be3e31614e97d
A-gen <agen@seven-words.info>
2015-12-17 17:25:39 +0900

    Solve Problem 0006

---
+++ lib/problems/0006.ex
@@ -0,0 +1,25 @@
+defmodule ExEuler.Problems.Problem0006 do
+  @moduledoc """
+  # Sum square difference
+  The sum of the squares of the first ten natural numbers is,
+    1^2 + 2^2 + ... + 10^2 = 385
+  The square of the sum of the first ten natural numbers is,
+    (1 + 2 + ... + 10)^2 = 55^2 = 3025
+  Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+  Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+  ## Example 1
+  iex> ExEuler.Problems.Problem0006.run(10)
+  2640
+
+  ## Answer
+  iex> ExEuler.Problems.Problem0006.run(100)
+  25164150
+  """
+
+  def run(num) do
+    sum = 1..num |> Enum.reduce(0, fn x, acc -> acc + x * x end)
+    pow = trunc 1..num |> Enum.reduce(0, &+/2) |> :math.pow(2)
+    pow - sum
+  end
+end
