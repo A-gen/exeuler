54398d56b7e72611bc2bffc270f3a10b4aa32960
A-gen <agen@seven-words.info>
2016-01-21 17:19:36 +0900

    Solve P0025

---
+++ lib/problems/0025.ex
@@ -0,0 +1,39 @@
+defmodule ExEuler.Problems.P0025 do
+  @moduledoc """
+  # 1000-digit Fibonacci number
+  The Fibonacci sequence is defined by the recurrence relation:
+    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+  Hence the first 12 terms will be:
+    F1 = 1
+    F2 = 1
+    F3 = 2
+    F4 = 3
+    F5 = 5
+    F6 = 8
+    F7 = 13
+    F8 = 21
+    F9 = 34
+    F10 = 55
+    F11 = 89
+    F12 = 144
+  The 12th term, F12, is the first term to contain three digits.
+
+  What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+  ## Example
+  iex> ExEuler.Problems.P0025.solve(3)
+  12
+
+  ## Answer
+  iex> ExEuler.Problems.P0025.solve
+  4782
+  """
+
+  def solve(digits \\ 1000) do
+    (
+      ExEuler.Math.fibs
+      |> Stream.take_while(&((&1 |> Integer.to_string |> String.length) < digits))
+      |> Enum.count
+    ) + 1
+  end
+end
